fab895045d6087d87a8e7baa3f853c8de1df29e6
humzahkiani <89326566+humzahkiani@users.noreply.github.com>
2025-12-08 21:28:25 -0800

    day 2 - part 2

--- day-2/solution.py
+++ day-2/solution.py
@@ -29,11 +29,11 @@ def is_invalid(id):
     return False
 
 
-print(f"{is_invalid(123123)} = True")
-print(f"{is_invalid(11122)} = False")
-print(f"{is_invalid(1)} = False")
-print(f"{is_invalid(11)} = True")
-print(f"{is_invalid(12)} = False")
+# print(f"{is_invalid(123123)} = True")
+# print(f"{is_invalid(11122)} = False")
+# print(f"{is_invalid(1)} = False")
+# print(f"{is_invalid(11)} = True")
+# print(f"{is_invalid(12)} = False")
 
 def part_one():
     ranges = parse_input()
@@ -49,4 +49,53 @@ def part_one():
 
 print(f"Part 1 - The sum of the invalid ids is {part_one()}")
 
+# Part 2
 
+def part_two():
+    ranges = parse_input()
+    invalid_id_sum = 0
+
+    for rang in ranges:
+        for id in range(rang[0],rang[1]+1):
+            if is_invalid(id):
+                invalid_id_sum += id
+
+    return invalid_id_sum
+
+
+def can_be_divided_into_n_equal_chunks(id, length, n):
+    str_id = str(id)
+
+    chunk_size, remainder = divmod(length, n)
+    if remainder != 0:
+        return False
+
+    chunks_idx = [[x,x+chunk_size] for x in range(0,length,chunk_size)]
+    unique_chunks = set()
+    for left,right in chunks_idx:
+        chunk = str_id[left:right]
+        unique_chunks.add(chunk)
+
+    return False if len(unique_chunks) > 1 else True
+
+
+def is_invalid(id):
+    str_id = str(id)
+    num_digits = len(str_id)
+
+    for divisions in range(2,num_digits+1):
+        if can_be_divided_into_n_equal_chunks(id, num_digits, divisions):
+            return True
+
+    return False
+
+
+print(f"Part 2 - The sum of the invalid ids is {part_two()}")
+
+# print(f"{is_invalid(123123)} = True")
+# print(f"{is_invalid(11122)} = False")
+# print(f"{is_invalid(1)} = False")
+# print(f"{is_invalid(11)} = True")
+# print(f"{is_invalid(12121212)} = True")
+# print(f"{is_invalid(77777)} = True")
+# print(f"{is_invalid(123123123)} = True")
